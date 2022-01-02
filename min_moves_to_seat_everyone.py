ea6d843f450e9781f6fabf7f028a30a10f054a41
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-01-01 23:05:13 -0800

    Implement min_moves_to_seat

--- min_moves_to_seat_everyone.py
+++ min_moves_to_seat_everyone.py
@@ -1,3 +1,11 @@
+from typing import List
+
+
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        
+        seats.sort()
+        students.sort()
+        sum = 0
+        for seat, student in zip(seats, students):
+            sum += abs(seat - student)
+        return sum
